723dd8fc30ec7e3fd99cb8002eedaa6f69712b3e
Remu Ollinen <remu.ollinen@gmail.com>
2022-08-17 22:13:25 +0300

    added a types file for frequently used types, also new colors

---
+++ types.ts
@@ -0,0 +1,11 @@
+/*
+ * This files contains types and interfaces that are used in several components
+ */
+
+export interface Project {
+	id: string;
+	name: string;
+	url: string;
+	rating: number;
+	created_at: string;
+}
